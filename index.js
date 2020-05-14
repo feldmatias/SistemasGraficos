b069f375b623170548a71636a0eecd3d1c6f792d
feldmatias <feldmatias@gmail.com>
2020-05-14 17:14:27 -0300

    refactor common operations in an onject Scene

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import {WebGL} from "./webgl/WebGL.js";
-import {FiguraCompuesta} from "./objects/figuracompuesta.js";
+import {Scene} from "./scene/Scene.js";
 
 let gl;
-let figura;
+let scene;
 
 function initWebGL() {
     const canvas = document.getElementById("my-canvas");
@@ -15,6 +15,7 @@ function initWebGL() {
 
     if (gl) {
         gl.setup(canvas.width, canvas.height);
+        scene = new Scene(gl);
         loadShaders();
 
     } else {
@@ -28,7 +29,6 @@ function loadShaders() {
     $.when(loadVertexShader(), loadFragmentShader()).done(function (res1, res2) {
         //this code is executed when all ajax calls are done
         gl.initShaders(vertexShaderSource, fragmentShaderSource);
-        figura = new FiguraCompuesta(gl);
         tick();
     });
 
@@ -51,29 +51,9 @@ function loadShaders() {
     }
 }
 
-function drawScene() {
-
-    gl.setProjection(30, 1920 / 1080, 0.1, 100.0);
-
-    let lightPosition = [10.0, 0.0, 3.0];
-    let ambientColor = [0.6, 0.6, 0.6];
-    let directionalColor = [1.2, 1.1, 0.7];
-    gl.setLighting(lightPosition, ambientColor, directionalColor);
-
-    var cameraDistance = 3;
-    var cameraHeight = 0.3;
-    gl.setView(cameraDistance, cameraHeight);
-
-    figura.draw();
-}
-
 function tick() {
     requestAnimFrame(tick);
-
-    // acumulo rotaciones en matrizModelado
-    figura.rotate(0.03 * 0.15, 0, 1, 0);
-
-    drawScene();
+    scene.draw();
 }
 
 window.onload = initWebGL;
---
+++ scene/Scene.js
@@ -0,0 +1,43 @@
+import {FiguraCompuesta} from "../objects/figuracompuesta.js";
+
+export class Scene {
+
+    constructor(gl) {
+        this.gl = gl;
+        this.createSceneObjects()
+    }
+
+    createSceneObjects() {
+        this.mainObject = new FiguraCompuesta(this.gl);
+    }
+
+    draw() {
+        this.mainObject.rotate(0.03 * 0.15, 0, 1, 0); // Apply angular velocity
+
+        this.setProjection();
+        this.setView();
+        this.setLighting();
+        this.mainObject.draw();
+    }
+
+    setProjection() {
+        let verticalFieldOfView = 30;
+        let aspect = 1920 / 1080;
+        let near = 0.1;
+        let far = 100.0;
+        this.gl.setProjection(verticalFieldOfView, aspect, near, far);
+    }
+
+    setView() {
+        let cameraDistance = 3;
+        let cameraHeight = 0.3;
+        this.gl.setView(cameraDistance, cameraHeight);
+    }
+
+    setLighting() {
+        let lightPosition = [10.0, 0.0, 3.0];
+        let ambientColor = [0.6, 0.6, 0.6];
+        let directionalColor = [1.2, 1.1, 0.7];
+        this.gl.setLighting(lightPosition, ambientColor, directionalColor);
+    }
+}
