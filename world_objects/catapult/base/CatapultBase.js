9a7922c2a8e419dfea8363e9969e82df636a8399
Matias Feld <feldmatias@gmail.com>
2020-05-25 15:28:13 -0300

    fixed catapult base size

--- world_objects/catapult/base/CatapultBase.js
+++ world_objects/catapult/base/CatapultBase.js
@@ -5,7 +5,7 @@ import {Colors} from "../../../scene/Colors.js";
 
 export class CatapultBase extends DrawableObject {
 
-    constructor(width = 4, length = 8) {
+    constructor(width = 5, length = 10) {
         super();
 
         this.base = new Cube(width, 0.3, length);
@@ -24,13 +24,14 @@ export class CatapultBase extends DrawableObject {
 
     createAxles(size, length) {
         let axle = new CatapultAxle(size);
+        let translation = length / 2 - 1.25;
 
         this.axleFront = axle.clone();
-        this.axleFront.translate(0, 0, length / 4);
+        this.axleFront.translate(0, 0, translation);
         this.axleFront.rotate(Math.PI / 2, 0, 1, 0);
 
         this.axleBack = axle.clone();
-        this.axleBack.translate(0, 0, -length / 4);
+        this.axleBack.translate(0, 0, -translation);
         this.axleBack.rotate(Math.PI / 2, 0, 1, 0);
     }
 }
